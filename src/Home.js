360eef7f7157ca98fb5a62589bb20dc87d1f2326
natashabuck <natashabuck1@gmail.com>
2019-03-02 13:27:59 +0000

    animation added to homepage

--- src/Home.js
+++ src/Home.js
@@ -1,13 +1,31 @@
 import React, { Component } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import './Styling/Home.scss'
 
 export default class Home extends Component {
   render() {
     return (
       <div className="homeContainer">
-      <h1>TinyBox</h1>
-      <h4>unique space in the uk.</h4>
+        <h1>TinyBox</h1>
+
+<div className="content">
+      <div className="content__container">
+
+      <ul class="content__container__list">
+      <li class="content__container__list__item">event</li>
+      <li class="content__container__list__item">workspace</li>
+      <li class="content__container__list__item">conference</li>
+      <li class="content__container__list__item">office party</li>
+      <li class="content__container__list__item">offsite</li>
+      <li class="content__container__list__item">training</li>
+      <li class="content__container__list__item">screening</li>
+      <li class="content__container__list__item">anything</li>
+      </ul>
+      </div>
+      <p className="content__container__text"> space in the uk.</p>
+    </div>
+
       <Form>
         <Form.Group>
           <div id="locationSearch">
@@ -21,6 +39,7 @@ export default class Home extends Component {
       <Button href="/venues" variant="primary" type="submit">
       Browse All Listings
     </Button>
+    
     </div>
     );
   }
